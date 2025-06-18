546633a2725ea2f4000eba3390589758b82ed7c3
texashodlr <conorxdevlin@gmail.com>
2025-06-17 20:55:59 -0600

    PDF uploader pod and script

--- minikube/start_kube.sh
+++ minikube/start_kube.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 minikube start --driver=docker --container-runtime=docker --gpus all
+
+kubectl apply -f k8s/yaml/pdf-data-pv.yaml
+kubectl apply -f k8s/yaml/pdf-data-pvc.yaml
+kubectl apply -f k8s/yaml/pdf_uploader
+kubectl cp ./data/pdfs/. data-prep/pdf-uploader:/data
