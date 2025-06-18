c554bd128a6d0451d64ac722cbec5b1b26bc525d
texashodlr <conorxdevlin@gmail.com>
2025-06-17 21:00:55 -0600

    Fixing minikube script

--- minikube/start_kube.sh
+++ minikube/start_kube.sh
@@ -2,7 +2,8 @@
 
 minikube start --driver=docker --container-runtime=docker --gpus all
 
+kubectl create ns data-prep
 kubectl apply -f k8s/yaml/pdf-data-pv.yaml
 kubectl apply -f k8s/yaml/pdf-data-pvc.yaml
-kubectl apply -f k8s/yaml/pdf_uploader
+kubectl apply -f k8s/yaml/pdf_uploader.yaml
 kubectl cp ./data/pdfs/. data-prep/pdf-uploader:/data
