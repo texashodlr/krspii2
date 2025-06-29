e622486f6e31680aea28de34774e2f7da23b77b7
texashodlr <conorxdevlin@gmail.com>
2025-06-28 22:04:01 -0600

    Updated minikube script

--- minikube/start_kube.sh
+++ minikube/start_kube.sh
@@ -11,9 +11,9 @@ echo "Directory '$pdf_data' created (if it didn't already exist)."
 minikube start --driver=docker --container-runtime=docker --gpus all
 
 kubectl create ns data-prep
-kubectl apply -f k8s/yaml/pdf-data-pv.yaml
-kubectl apply -f k8s/yaml/pdf-data-pvc.yaml
-kubectl apply -f k8s/yaml/pdf_loader.yaml
+kubectl apply -f k8s/yaml/stage_one/pdf-data-pv.yaml
+kubectl apply -f k8s/yaml/stage_one/pdf-data-pvc.yaml
+kubectl apply -f k8s/yaml/stage_one/pdf_loader.yaml
 
 # Wait until the pdf-loader pod is ready
 echo "Waiting for pod pdf-uploader in data-prep to be ready..."
@@ -28,4 +28,30 @@ kubectl cp ./data/pdfs/. data-prep/pdf-uploader:/data/pdfs/pdfs
 kubectl -n data-prep exec pdf-uploader -- sh -c "ls -lh /data/pdfs/pdfs"
 
 # Applying the yaml for PDF to JSONL Job.
-# kubectl apply -f k8s/yaml/preprocess_pdfs_job.yaml
+kubectl apply -f k8s/yaml/stage_one/preprocess_pdfs_job.yaml
+
+# Validating that the processed PDFs are occuring.
+echo "Waiting for the PDF to JSONL processing to complete..."
+target_job=4
+target_count=24
+while true; do
+	# Jobs
+
+	completed=$(kubectl -n data-prep get job pdf-preprocess -o jsonpath='{.status.succeeded}')
+	completed=${completed:-0}
+	echo "Current completions: $completed"
+	if ["$completed" -eq "$target_job"]; then
+		echo "Processing job has reached $target completions."
+	fi
+
+	file_count=$(kubectl -n data-prep exec pdf-uploader -- sh -c "ls -l /data/pdfs/processed | wc -l")
+	#file_count=$(ls -l "/data/pdfs/processed" | wc -l)
+	echo "Current file count: $file_count"
+	if ["$file_count" -eq "$target_count"]; then
+		echo "Directory now has $target_count files!"
+		echo -n "."
+		sleep 2
+		break
+	fi
+done
+echo "All PDFs processed!"
